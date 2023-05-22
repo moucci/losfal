7ddd263db57bff6a91d013f9bc52835ee1796cd5
= <=>
2023-05-22 16:28:29 +0200

    update index.html with splash screen and home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,37 @@
-console.log('hello word')
+/**
+ * container splash screen
+ * @type {Element}
+ */
+let $splashScreen = document.querySelector('.loader');
+/**
+ * list of letter
+ * @type {NodeListOf<Element>}
+ */
+let $logoTxtChild = $splashScreen.querySelectorAll('.logo-txt-b span');
+/**
+ * index Of Letter for logo-txt-b span
+ * @type {number}
+ */
+let logoTxtChildNumber = -1;
+let effectInterval = setInterval(() => {
+    if (logoTxtChildNumber === 5) {
+        clearInterval(effectInterval);
+        $splashScreen.querySelector('.logo-txt-b').remove();
+
+        setTimeout(() => {
+            // $splashScreen.remove();
+            $splashScreen.style.opacity = 0;
+            document.querySelector('main').style.display = 'block';
+
+            $splashScreen.addEventListener('transitionend', function () {
+                $splashScreen.remove();
+            })
+        }, 3000);
+    }else{
+        logoTxtChildNumber++;
+        $logoTxtChild[logoTxtChildNumber].style.opacity = 1;
+    }
+
+
+
+}, 500)
